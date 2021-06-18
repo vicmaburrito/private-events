6e8297b2f0735e85b569e908ad00f5bfa744a07f
Daniel Samuel <dansam5000@gmail.com>
2021-06-18 10:45:52 +0100

    Add Active record association for all model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :events
+  has_many :events, inverse_of: :creator
+  has_many :enrollments
+  has_many :attended_events, through: :enrollments, source: :event
 end
