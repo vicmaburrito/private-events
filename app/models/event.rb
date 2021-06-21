d35aeca4da3949ce893facaaeb56ff00759995ef
vicmaburrito <vmaldaraca@gmail.com>
2021-06-21 13:30:42 -0500

    Fix linter

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :user
-  scope :past, -> {where('date < ?', DateTime.now)}
-  scope :future, ->{where('date >= ?', DateTime.now)}
+  scope :past, -> { where('date < ?', DateTime.now) }
+  scope :future, -> { where('date >= ?', DateTime.now) }
 end
