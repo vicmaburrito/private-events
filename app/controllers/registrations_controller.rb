eb01abfd11ff7ad070078dd1dbf7efd3764257a7
Daniel Samuel <dansam5000@gmail.com>
2021-06-21 22:28:02 +0100

    mAdd descriptive ReadMe few fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,11 @@
-private
+class RegistrationsController < ApplicationController
+  private
 
-def sign_up_params
-  params.require(:user).permit(:email, :password, :password_confirmation)
-end
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 
-def account_update_params
-  params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
+  def account_update_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
+  end
 end
