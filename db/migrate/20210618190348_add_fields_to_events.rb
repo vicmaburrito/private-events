7e276115d975b2bb890a9712cca54e87fd99b780
vicmaburrito <vmaldaraca@gmail.com>
2021-06-22 12:44:08 -0500

    Destroy models User,Event & enrollments

--- db/migrate/20210618190348_add_fields_to_events.rb
+++
@@ -1,8 +0,0 @@
-class AddFieldsToEvents < ActiveRecord::Migration[6.1]
-  def change
-    add_column :events, :location, :string
-    add_column :events, :description, :string
-    add_column :events, :date, :datetime 
-  end
-end
- 
