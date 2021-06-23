9165d83fac010d2afcc23ec0fb193bfb5f79d1af
Daniel Samuel <dansam5000@gmail.com>
2021-06-23 18:29:07 +0100

    Fix undefined date variable

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :enrollments
   has_many :attendees, through: :enrollments, source: :user
-  scope :past, -> { where('date < ?', Date.today) }
-  scope :upcoming, -> { where('date >= ?', Date.today) }
+  scope :past, -> { where('start_date < ?', Date.today) }
+  scope :upcoming, -> { where('start_date >= ?', Date.today) }
 end
