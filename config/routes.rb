1a2be19de926407012736b2576124d63717e4656
vicmaburrito <vmaldaraca@gmail.com>
2021-06-23 01:54:03 -0500

    Add GET routes in routes files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "events#index"
-
+  get 'signup' => 'users#new', as: 'signup'
+  get 'login' => 'sessions#new', as: 'login'
+  get 'logout' => 'sessions#destroy', as: 'logout'
   resources :events
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
