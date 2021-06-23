bfc1c88888d7d0608aab9a417ea6e49c639bb94d
vicmaburrito <vmaldaraca@gmail.com>
2021-06-23 12:11:35 -0500

    Add resource enrollments in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :events
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :enrollments, only: [:create, :destroy, :update]
+
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
