f83f5501681d73cfcfe874408de0379a9f4bfd02
vicmaburrito <vmaldaraca@gmail.com>
2021-06-22 21:56:12 -0500

    Add resources in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "events#index"
+
   resources :events
+  resources :users
+  resources :session, only: [:new, :create, :destroy]
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
