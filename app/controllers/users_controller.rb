7c8b9c2eff9d43d3e07f1a72bffdf4497df9c041
vicmaburrito <vmaldaraca@gmail.com>
2021-06-23 00:34:22 -0500

    Add basic CRUD in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+    def index 
+        @users = User.all
+    end
+    def new
+        @user = User.new
+    end 
+    def create
+        @user = User.new
+        @user.save
+        session[:user_id] = @user.id
+        flash.notice = "User #{@user.name} sucessfully created! Thank you for signing up!"
+        redirect_to user_path(@user)
+    end
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name)
+    end
+
 end
