00f419a7320d7fd83da860ab7b0dc975b95bb2a0
vicmaburrito <vmaldaraca@gmail.com>
2021-06-23 01:56:58 -0500

    Fix users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,23 @@
 class UsersController < ApplicationController
 
-    def index 
-        @users = User.all
-    end
-    def new
-        @user = User.new
-    end 
-    def create
-        @user = User.new
-        @user.save
-        session[:user_id] = @user.id
-        flash.notice = "User #{@user.name} sucessfully created! Thank you for signing up!"
-        redirect_to user_path(@user)
-    end
-    def show
-        @user = User.find(params[:id])
-    end
+before_action :require_login, except: [:new, :create]
 
-    def user_params
-      params.require(:user).permit(:name)
-    end
+def index
+  @users = User.all
+end
 
+def new
+  @user = User.new
+end
+
+def create
+  @user = User.new(user_params)
+  @user.save
+  session[:user_id] = @user.id
+  flash.notice = "User #{@user.name} successfully created! Thank you for signing up!"
+  redirect_to user_path(@user)
+end
+
+def show
+  @user = User.find(params[:id])
 end
