0b869d81083bc4a7bbd2e8096e441d12951211c9
vicmaburrito <vmaldaraca@gmail.com>
2021-06-22 22:22:00 -0500

    Delete users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   root "events#index"
 
   resources :events
-  resources :users
-  resources :session, only: [:new, :create, :destroy]
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
